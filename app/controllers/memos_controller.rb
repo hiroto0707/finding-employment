e3edb3650363a50df36bdbf95ed14f241f9a2917
hiroto0707 <77100499+hiroto0707@users.noreply.github.com>
2021-05-14 15:43:49 +0900

    編集・削除機能の実装完了

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,5 +1,6 @@
 class MemosController < ApplicationController
   before_action :authenticate_user!, only: [:index]
+  before_action :set_memo, only: [:edit, :update, :destroy]
   
   def index
     @memos = current_user.memos.order(industry_id: :desc).all
@@ -22,12 +23,24 @@ class MemosController < ApplicationController
   end
 
   def update
+    if @memo.update(memo_params)
+      redirect_to memos_path
+    else
+      render :edit
+    end  
   end
 
   def destroy
+    @memo.destroy
+    redirect_to memos_path
   end
 
   private
+
+  def set_memo
+    @memo = Memo.find(params[:id])
+  end
+
   def memo_params
     params.require(:memo).permit(:title, :selection_status, :body, :industry_id).merge(user_id: current_user.id)
   end
