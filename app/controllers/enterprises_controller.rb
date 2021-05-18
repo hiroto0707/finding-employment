99190dba47c00573389646e136f39424c1e0ca9d
hiroto0707 <77100499+hiroto0707@users.noreply.github.com>
2021-05-18 18:46:25 +0900

    投稿者のみが編集・削除ボタンが表示される条件分岐

--- app/controllers/enterprises_controller.rb
+++ app/controllers/enterprises_controller.rb
@@ -1,6 +1,9 @@
 class EnterprisesController < ApplicationController
+  before_action :set_enterprise, only: [:show, :edit, :update, :destroy]
+
   def index
     @enterprises = Enterprise.includes(:user).order(industry_id: :desc)
+    
   end
 
   def new
@@ -23,13 +26,25 @@ class EnterprisesController < ApplicationController
   end
 
   def update
+    if @enterprise.update(enterprise_params)
+      redirect_to enterprises_path
+    else
+      render :edit
+    end  
   end
 
   def destroy
+    @enterprise.destroy
+    redirect_to enterprises_path
   end
-  
+
 
   private
+
+  def set_enterprise
+    @enterprise = Enterprise.find(params[:id])
+  end
+
 
   def enterprise_params
     params.require(:enterprise).permit(:title, :text, :theme, :industry_id).merge(user_id: current_user.id)
