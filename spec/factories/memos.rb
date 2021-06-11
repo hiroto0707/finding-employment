e347b14e3e459ed44b5c5d69e736c1af1b525427
hiroto0707 <77100499+hiroto0707@users.noreply.github.com>
2021-06-11 15:02:55 +0900

    メモ機能のテストコード

--- spec/factories/memos.rb
+++ spec/factories/memos.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :memo do
-    
+    title                    {'ああああああああ'}
+    selection＿status        {'ああああああああ'}
+    body                     {'ああああああああ'}
+    industry_id              {2}
   end
 end
