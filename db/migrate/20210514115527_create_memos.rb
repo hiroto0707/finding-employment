62f2c0c838b1872382e5cc521e049fcf46df2309
hiroto0707 <77100499+hiroto0707@users.noreply.github.com>
2021-06-19 16:24:29 +0900

    マイグレーションファイルの編集

--- db/migrate/20210514115527_create_memos.rb
+++ db/migrate/20210514115527_create_memos.rb
@@ -5,6 +5,7 @@ class CreateMemos < ActiveRecord::Migration[6.0]
       t.text :selection_status,      null: false
       t.text :body,                   null: false
       t.integer :industry_id,         null: false
+      t.string :enterprise_url
       t.references :user,             null: false,foreign_key: true
       t.timestamps
     end
